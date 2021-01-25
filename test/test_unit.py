3eba8d1417fa4091031e65ea9dde39186519e787
David Wu <wu.david3283@gmail.com>
2021-01-26 10:43:44 +1100

    massive refactor for main script and tests

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,15 +1,43 @@
 import pytest
-import sys
-import os
-from importlib import reload
-
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 
-def test_prettify_with_documentation(tmpdir, util):
-    in_name = "count.s"
-    in_path = tmpdir.join(in_name)
-    content = '''
+from .. import mppd
+
+
+@pytest.fixture()
+def mips_main():
+    def do_main(args: list):
+        mppd.MipsProcessor(mppd.get_arg_parser().parse_args(args)).process()
+
+    return do_main
+
+
+@pytest.fixture()
+def assert_result(mips_main, tmpdir):
+    def do_test(mips_args, input_text: str, expected_text: str, filename: str = "test"):
+        in_path = tmpdir.join(f"{filename}.s")
+        out_path = tmpdir.join(f"{filename}.out.s")
+
+        # Write input to file
+        with in_path.open("w") as f:
+            f.write(input_text)
+
+        # Run preprocessor and prettifier
+        mips_main([str(in_path), "-o", str(out_path)] + mips_args)
+
+        # Check result matches expected output
+        with out_path.open("r") as f:
+            result = f.read().strip()
+
+        assert result == expected_text.strip()
+
+    return do_test
+
+
+def test_prettify_with_documentation(assert_result):
+    assert_result(
+        ["-f", "count", "-d", "-s", "-l", "-p", "-i"],
+        '''
 count:
     li      %max.s, 10                  # int max = 10;
 count_i_init:
@@ -21,38 +49,34 @@ count_i_step:
     addi    %i,%i,1                     # i++;
 count_i_break:
     jr      $ra                         # return;
-    '''
-    with in_path.open("w") as f:
-        f.write(content)
-    out_name = "count.out.s"
-    out_path = tmpdir.join(out_name)
-    sys.argv = ['mips.py', str(in_path), "-f", "count", "-o", str(out_path), "-d", "-s", "-l", "-p", "-i"]
-    import mips
-    with out_path.open("r") as f:
-        result = util.remove_whitespace(f.read())
-    expected = util.remove_whitespace(
+            ''',
         '''#########################
-        #count
-        #Frame:$ra,$s0
-        #Uses:$s0,$t0
-        #Clobbers:$t0
-        #Locals:
-        #-'max'in$s0
-        #-'i'in$t0
-        #Structure:
-        #-count_i_init
-        #-count_i_cond
-        #-count_i_step
-        #-count_i_break
-        count:
-        li$s0,10#intmax=10;
-        count_i_init:
-        li$t0,0#inti=0;
-        count_i_cond:
-        bge$t0,$s0,count_i_break#for(i<max)
-        #...
-        count_i_step:
-        addi$t0,$t0,1#i++;
-        count_i_break:
-        jr$ra#return;''')
-    assert result == expected
+# count
+
+# Frame:      $ra, $s0
+# Uses:       $s0, $t0
+# Clobbers:   $t0
+
+# Locals:
+#       - 'max' in $s0
+#       - 'i' in $t0
+
+# Structure:
+#       - count_i_init
+#       - count_i_cond
+#       - count_i_step
+#       - count_i_break
+count:
+    li        $s0, 10                               # int max = 10;
+count_i_init:
+    li        $t0, 0                                # int i = 0;
+count_i_cond:
+    bge       $t0, $s0, count_i_break               # for (i < max)
+    # ...
+count_i_step:
+    addi      $t0, $t0, 1                           # i++;
+count_i_break:
+    jr        $ra                                   # return;
+        ''',
+        "count"
+    )
